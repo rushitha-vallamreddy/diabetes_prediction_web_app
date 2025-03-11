83f1d749dae6160cdb906532c077eb6a9401887e
RUSHITHA_VALLAMREDDY <sahirushi9@gmail.com>
2025-03-11 12:22:39 +0530

    Add files via upload

---
+++ predictive system.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 11 11:24:07 2025
+
+@author: rushi
+"""
+
+import numpy as np
+import pickle
+
+loaded_model = pickle.load(open(r"C:\Users\rushi\Desktop\models\diabetes_model.sav", "rb"))
+
+
+
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+
+prediction =loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
+  
